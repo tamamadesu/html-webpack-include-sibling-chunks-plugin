a9d3ab351007d0601538456ee47a9744d0eed380
tamamadesu <liyujie@mmmono.com>
2018-04-12 17:40:08 +0800

    add comments

--- HtmlWebpackIncludeSiblingChunksPlugin.js
+++ HtmlWebpackIncludeSiblingChunksPlugin.js
@@ -21,6 +21,8 @@ class HtmlWebpackIncludeSiblingChunksPlugin {
 
       compilation.hooks.htmlWebpackPluginAlterChunks.tap('HtmlWebpackIncludeSiblingChunksPlugin', chunks => {
         const ids = [].concat(...chunks.map(chunk => [...chunk.siblings,chunk.id]))
+
+        // 目前还是有点问题，如果某个 entry 没有用到 splitChunks 还是会被引入，翻了会源码，暂时还没找到很好的办法解决，标记一下
         return allChunks.filter(chunk => ids.includes(chunk.id)).sort((a,b)=> ids.join('').indexOf(a.id + b.id) !== -1 ? -1 : 1)
       })
     })
